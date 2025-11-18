eeac4e27e886896580be20279f3da56ef446590f
ninadchaphekar <ninadchaphekar@gmail.com>
2025-11-18 14:55:21 +0530

    Modified to save plots

--- agent.py
+++ agent.py
@@ -5,10 +5,12 @@ from collections import deque
 from game import SnakeGameAI, Direction, Point
 from model import Linear_QNet, QTrainer
 from helper import plot
+import matplotlib.pyplot as plt
 
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
 LR = 0.001
+MAX_GAMES = 1000
 
 class Agent:
 
@@ -141,6 +143,22 @@ def train():
             mean_score = total_score / agent.n_games
             plot_mean_scores.append(mean_score)
             plot(plot_scores, plot_mean_scores)
+            if agent.n_games % 250 == 0:
+                plt.clf()
+                plt.title(f'Trained for {agent.n_games} games')
+                plt.xlabel('Number of Games')
+                plt.ylabel('Score')
+                plt.plot(plot_scores)
+                plt.plot(plot_mean_scores)
+                plt.ylim(ymin=0)
+                plt.text(len(plot_scores)-1, plot_scores[-1], str(plot_scores[-1]))
+                plt.text(len(plot_mean_scores)-1, plot_mean_scores[-1], str(plot_mean_scores[-1]))
+                plt.show()
+                plt.savefig(f'plots/trained_{agent.n_games}games_plot.png')
+                print(f"Successfully saved training plot after {agent.n_games} games!")
+                if agent.n_games >= MAX_GAMES:
+                    break
+
 
 
 if __name__ == '__main__':
